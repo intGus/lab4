52e928eefb37e1f605576619c599e9e5890c9064
intGus <gustavodiazm@gmail.com>
2020-04-09 22:18:42 +0000

    Completed report_email script, working as intended, fixed bugs on emails.py

--- report_email.py
+++ report_email.py
@@ -3,6 +3,7 @@
 import reports
 import os
 from datetime import date
+import emails
 
 def process_data():
   path = "../supplier-data/descriptions/"
@@ -12,7 +13,7 @@ def process_data():
     #print(file)
     with open(path + file) as fh:
       lines = fh.readlines()
-      paragraph = paragraph + lines[0].strip() + "<br/>" + lines[1].strip() + "<br/><br/>"
+      paragraph = paragraph + "name: " + lines[0].strip() + "<br/>" + "weight: " + lines[1].strip() + "<br/><br/>"
 
   return paragraph
 
@@ -21,6 +22,15 @@ def main():
   title = "Processed Update on " + date.today().strftime("%B %d, %Y")
   #print(title) 
   reports.generate_report('/tmp/processed.pdf',title, string)
+
+  #emailing
+  sender = "automation@example.com"
+  receiver = "{}@example.com".format(os.environ.get('USER'))
+  subject = "Upload Completed - Online Fruit Store"
+  body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+
+  message = emails.generate_email(sender,receiver,subject,body, '/tmp/processed.pdf')
+  emails.send_email(message)
 
 if __name__ == "__main__":
   main()
