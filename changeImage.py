6e81df000f56f09a020c85be8349e6196b8f7cc4
intGus <gustavodiazm@gmail.com>
2020-04-09 19:44:55 +0000

    Completes the changeImage.py, currently working as intended

---
+++ changeImage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+path = "../supplier-data/images/"
+
+files = os.listdir(path)
+
+for file in files:
+  print(file)
+  try:
+    file_name, ext = file.split('.')
+    im = Image.open(path+file)
+    new_im = im.resize((600,400)).convert("RGBA")
+    background = Image.new('RGBA', new_im.size, (255,255,255))
+    final = Image.alpha_composite(background, new_im).convert("RGB")
+    final.save(path + file_name + '.jpeg', 'JPEG', quality=100)
+  except:
+    continue
+
