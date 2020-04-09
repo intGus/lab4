28f857c86b6fdaabb30ab40f2a1c401cade63a1b
intGus <gustavodiazm@gmail.com>
2020-04-09 19:48:08 +0000

    Added comments to the changeImage.py

--- changeImage.py
+++ changeImage.py
@@ -10,9 +10,9 @@ files = os.listdir(path)
 for file in files:
   print(file)
   try:
-    file_name, ext = file.split('.')
+    file_name, ext = file.split('.') #get the file name for renaming
     im = Image.open(path+file)
-    new_im = im.resize((600,400)).convert("RGBA")
+    new_im = im.resize((600,400)).convert("RGBA") #processing the alpha channel
     background = Image.new('RGBA', new_im.size, (255,255,255))
     final = Image.alpha_composite(background, new_im).convert("RGB")
     final.save(path + file_name + '.jpeg', 'JPEG', quality=100)
